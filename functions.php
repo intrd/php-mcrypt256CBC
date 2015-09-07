901fd4645e75026cf917f96216994def059cefd2
intrd <x@dann.com.br>
2015-09-07 16:27:39 -0300

    license

--- functions.php
+++ functions.php
@@ -6,10 +6,10 @@
 * @version 1.0
 * @category functions
 * @author intrd - http://dann.com.br
-* @link https://github.com/intrd/php-mcrypt256CBC/
 * @see inspired by joshhartman http://www.warpconduit.net/2013/04/14/highly-secure-data-encryption-decryption-made-easy-with-php-mcrypt-rijndael-256-and-cbc/
 * @copyright 2015 intrd
 * @license http://creativecommons.org/licenses/by/4.0/
+* @link https://github.com/intrd/php-mcrypt256CBC/
 */
 
 /**
